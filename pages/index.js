00eafc0abcbf186b1fca4f02cb6c1b8a2816f7bf
Luis Flores Álvarez <luisfloresdev@gmail.com>
2020-06-07 14:41:17 -0500

    Add redirect

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,21 @@ import Header from '../components/Header';
 import Hero from '../components/Hero';
 import Benefits from '../components/Benefits';
 import Sign from '../components/Sign';
+import { useEffect } from 'react';
+import Router from 'next/router';
 
 export default function Home() {
+  useEffect(() => {
+    const sessionData = JSON.parse(localStorage.getItem('session'));
+    if (sessionData) {
+      if (sessionData.user.typeOfUser === 'mentor') {
+        Router.push('/mentor');
+      } else {
+        Router.push('/pymes');
+      }
+    }
+  });
+
   return (
     <div className="container">
       <Head>
